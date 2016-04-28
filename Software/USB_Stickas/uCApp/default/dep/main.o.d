992bac108ce6889a62880d2b0ec8ad64cce6a5b1
Dravigas <alenaskriau@gmail.com>
2016-04-28 20:31:06 +0300

    USB_STICKAS

---
+++ Software/USB_Stickas/uCApp/default/dep/main.o.d
@@ -0,0 +1,69 @@
+default/main.o: main.c conf/config.h conf/../lib_mcu/compiler.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iousb162.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iousbxx2.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  conf/conf_scheduler.h modules/scheduler/scheduler.h \
+  lib_mcu/wdt/wdt_drv.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/wdt.h \
+  lib_mcu/power/power_drv.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/power.h \
+  lib_mcu/usb/usb_drv.h lib_mcu/util/start_boot.h
+
+conf/config.h:
+
+conf/../lib_mcu/compiler.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iousb162.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iousbxx2.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+conf/conf_scheduler.h:
+
+modules/scheduler/scheduler.h:
+
+lib_mcu/wdt/wdt_drv.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/wdt.h:
+
+lib_mcu/power/power_drv.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/power.h:
+
+lib_mcu/usb/usb_drv.h:
+
+lib_mcu/util/start_boot.h:
